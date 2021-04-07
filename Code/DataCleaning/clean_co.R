4bbdf9d3d11731440bbc40d4101c5fdf311c400a
Laura Lazarescou <54220360+lalazarescou@users.noreply.github.com>
2021-04-06 21:12:14 -0500

    Added non-scaled cols, standardize file name and location

--- Code/DataCleaning/clean_co.R
+++ Code/DataCleaning/clean_co.R
@@ -3,22 +3,27 @@
 library(dplyr)
 library(BBmisc)
 
+file_direct = "C:/SMU_Local/data/capstone/Data/GIS_PACKAGE_FILES_TO_CSV/"
+
 # add co features
 cert_occupancy = read.csv(file=paste0(file_direct, 'Clipped_Parcels_by_Dallas_Simple_inner_join_to_Clipped_2019_co.csv'), stringsAsFactors = FALSE)
 cert_occupancy_features = cert_occupancy %>%
   mutate(CO_Issue_Date = as.Date(ISSUE.DATE, "%m/%d/%Y")) %>%
-  mutate(Days_From_CO_Appro_To_Issue = normalize(as.numeric(CO_Issue_Date - as.Date(DATE.APPRO, "%m/%d/%Y"), units="days"), method="scale")) %>%
-  mutate(Days_Since_Issue = normalize(as.numeric(Sys.Date() - CO_Issue_Date, units="days"), method="scale")) %>%
-  mutate(Sq_Ft = normalize(SQ.FT, method="scale")) %>%
+  mutate(days_from_CO_appro_to_issue = as.numeric(CO_Issue_Date - as.Date(DATE.APPRO, "%m/%d/%Y"), units="days")) %>%
+  mutate(days_from_CO_appro_to_issue_scaled = normalize(days_from_CO_appro_to_issue, method="scale")) %>%
+  mutate(days_since_issue = as.numeric(as.Date("2021-01-01") - CO_Issue_Date, units="days")) %>%
+  mutate(days_since_issue_scaled = normalize(days_since_issue, method="scale")) %>%
+  mutate(sq_ft = SQ.FT) %>%
+  mutate(sq_ft_scaled = normalize(sq_ft, method="scale")) %>%
   group_by(Acct) %>%
-  add_tally(name="Count_COs") %>%
-  ungroup() %>%
-  mutate(Count_COs = normalize(Count_COs, method="scale")) %>%
+  add_tally(name="count_COs") %>%
   filter(CO_Issue_Date == max(CO_Issue_Date)) %>%
-  mutate(Occupancy = as.numeric(factor(OCCUPANCY))) %>%
-  mutate(CO_Code_Distr = as.numeric(factor(CODE.DISTR))) %>%
-  mutate(CO_Type = as.numeric(factor(TYPE.OF.CO))) %>%
-  select(Acct, Days_From_CO_Appro_To_Issue, Count_COs, Days_Since_Issue, CO_Type, Sq_Ft, Occupancy, CO_Code_Distr)
+  ungroup() %>%
+  mutate(count_COs_scaled = normalize(count_COs, method="scale")) %>%
+  mutate(occupancy = as.numeric(factor(OCCUPANCY))) %>%
+  mutate(CO_code_distr = as.numeric(factor(CODE.DISTR))) %>%
+  mutate(CO_type = as.numeric(factor(TYPE.OF.CO))) %>%
+  select(Acct, days_from_CO_appro_to_issue, days_from_CO_appro_to_issue_scaled, count_COs, count_COs_scaled, days_since_issue, days_since_issue_scaled, CO_type, sq_ft, sq_ft_scaled, occupancy, CO_code_distr)
 
 # Write the data set to a file
-write.csv(cert_occupancy_features, "Data/Derived/df_cert_occupancy.csv")
+write.csv(cert_occupancy_features, "../../Data/clean_cert_occupancy.csv")
