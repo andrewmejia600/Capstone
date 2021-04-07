823177b3dad5ed24789b25ae13056760b241122a
Laura Lazarescou <54220360+lalazarescou@users.noreply.github.com>
2021-04-06 21:13:10 -0500

    added non-scaled col, standardize output file location

--- Code/DataCleaning/clean_crime.R
+++ Code/DataCleaning/clean_crime.R
@@ -1,12 +1,14 @@
-getwd()
-setwd('C:/Users/sabri/Documents/Masters/CAPSTONE')
+#
+# Prepare clean_crime.csv based on City of Dallas crime data
+
+file_direct = "C:/SMU_Local/data/capstone/Data/GIS_PACKAGE_FILES_TO_CSV/"
 
 library(dplyr)
 library(tidyverse)
 library(superml)
 
-#target file output is 'clean_crime.r'
-df <- read.csv('Clipped_Parcels_by_Dallas_Simple_inner_join_to_Clipped_2019_Group_A_Crime.csv', header=TRUE)
+
+df <- read.csv(file=paste0(file_direct,'Clipped_Parcels_by_Dallas_Simple_inner_join_to_Clipped_2019_Group_A_Crime.csv'))
 glimpse(df)
 dim(df)
 head(df)
@@ -26,6 +28,6 @@ new_df$nibrs_crime_against <- label$fit_transform(new_df$nibrs_crime_against)
 
 #scaling the data
 x = new_df$count_of_crime
-new_df$count_of_crime <- scale(x, center = FALSE)
+new_df$count_of_crime_scaled <- scale(x, center = FALSE)
 
-write.csv(new_df, 'clean_crime.csv')
+write.csv(new_df, '../../Data/clean_crime.csv')
