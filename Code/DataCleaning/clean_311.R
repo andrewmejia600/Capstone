5196a4d935b2520c842eccd92fe1cb85bf227691
Laura Lazarescou <54220360+lalazarescou@users.noreply.github.com>
2021-04-06 21:11:32 -0500

    Added non-scaled cols, standardized file names and location reference

--- Code/DataCleaning/clean_311.R
+++ Code/DataCleaning/clean_311.R
@@ -1,15 +1,11 @@
-getwd()
-setwd('C:/Users/sabri/Documents/Masters/CAPSTONE')
+
+#  Clean 311 Code data from City of Dallas, TX
 
 library(dplyr)
 library(tidyverse)
+file_direct = 'C:/SMU_Local/data/capstone/Data/GIS_PACKAGE_FILES_TO_CSV/'
 
-
-#target file output is 'clean_crime.r'
-df <- read.csv('Clipped_Parcels_by_Dallas_Simple_inner_join_to_Clipped_2019_311.csv', header=TRUE)
-glimpse(df)
-dim(df)
-
+df = read.csv(file=paste0(file_direct,'Clipped_Parcels_by_Dallas_Simple_inner_join_to_Clipped_2019_311.csv'))
 
 new_df <- df %>% count(Shape_STAr, Shape_STLe, Acct, sort = TRUE)
 new_df$count_of_311 <- new_df$n #relabeling the count from n to something more specific
@@ -19,5 +15,6 @@ head(new_df)
 #scaling the data
 x = new_df$count_of_311
 
-new_df$count_of_311 <- scale(x, center = FALSE)
-write.csv(new_df, 'clean_311.csv')
+new_df$count_of_311_scaled = scale(x, center = FALSE)
+head(new_df)
+write.csv(new_df, '../../Data/clean_311.csv')
