cb088baf461cc7bf0ab006119eab57d7ed5d2874
Tina Pai <mail4tinapai@gmail.com>
2021-05-03 20:39:31 -0500

    Add summary stats to impr val eda

--- Code/EDA/impr_val_eda.R
+++ Code/EDA/impr_val_eda.R
@@ -3,8 +3,16 @@
 library(dplyr)
 library(ggplot2)
 
-df = read.csv("../../Data/df_log_and_scaled.csv")
+df = read.csv("../../Data/df_all.csv")
 
+# table for summary stats of improvement value of vacant parcels 
+summary(df$impr_val[df$vac_par == 1])
+# table for summary stats of improvement value of non-vacant parcels 
+summary(df$impr_val[df$vac_par == 0])
+
+# plot boxplots for log improvement value by vacant/nonvacant
 df %>%
-  ggplot(aes(x=factor(vac_par), y=log_impr_val)) +
-  geom_boxplot() 
+  ggplot(aes(x=factor(vac_par), y=log_impr_val, fill=factor(vac_par))) +
+  geom_boxplot() +
+  ggtitle("Log Improvement Value by Vacant/Non-Vacant") +
+  xlab("Vacant (1) or Non-Vacant (0)")
