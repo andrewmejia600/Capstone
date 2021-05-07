71ff1fdf9257ddd6cc0921a2f5d3feb7c39fe7cf
Tina Pai <mail4tinapai@gmail.com>
2021-05-07 11:21:39 -0700

    Delete rebucket_zoning.R

--- Code/DataCleaning/rebucket_zoning.R
+++
@@ -1,7 +0,0 @@
-df = read.csv("/Users/tina/Documents/School/Capstone/Dallas Files/DCAD2019_CERTIFIED_07252019/account_apprl_year.csv")
-
-df_land = read.csv("/Users/tina/Documents/School/Capstone/Dallas Files/DCAD2019_CERTIFIED_07252019/land.csv")
-summary(df_land$ZONING)
-
-c("FAMILY", "PLANNED DEVELOPMENT", "INDUSTRIAL", "OFFICE", "AGRICULTURAL", "COMMERCIAL", "RETAIL", "LIQUOR", "OTHER")
-
