8271b65b1c4846502355b7c49cd2c030c91cb220
Tina Pai <mail4tinapai@gmail.com>
2021-05-31 00:07:36 -0500

    aggregate 311 to count per account

--- Code/DataCleaning/clean_311.R
+++ Code/DataCleaning/clean_311.R
@@ -7,14 +7,11 @@ file_direct = 'C:/SMU_Local/data/capstone/Data/GIS_PACKAGE_FILES_TO_CSV/'
 
 df = read.csv(file=paste0(file_direct,'Clipped_Parcels_by_Dallas_Simple_inner_join_to_Clipped_2019_311.csv'))
 
-new_df <- df %>% count(Shape_STAr, Shape_STLe, Acct, sort = TRUE)
-new_df$count_of_311 <- new_df$n #relabeling the count from n to something more specific
-new_df <- select (new_df,-c(4))
+new_df <- df %>% 
+  group_by(Acct) %>%
+  summarise(count_of_311 = n()) %>% # count number of 311 aggregated by account number
+  mutate(count_of_311_scaled = scale(count_of_311, center = FALSE)[,1]) # scale the data
+  
 head(new_df)
 
-#scaling the data
-x = new_df$count_of_311
-
-new_df$count_of_311_scaled = scale(x, center = FALSE)
-head(new_df)
 write.csv(new_df, '../../Data/clean_311.csv')
