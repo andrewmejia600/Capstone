1d1b18edd23ac1c699a9a4f62a98ef3b6b4d210a
Tina Pai <mail4tinapai@gmail.com>
2021-05-31 15:56:06 -0500

    Fix duplicate building permit accounts

--- Code/DataCleaning/clean_building_permits.R
+++ Code/DataCleaning/clean_building_permits.R
@@ -3,8 +3,8 @@
 library(dplyr)
 library(BBmisc)
 
-#file_direct = "/Users/tina/Documents/School/Capstone/Dallas Files/GIS_PACKAGE_FILES_TO_CSV/"
-file_direct = 'C:/SMU_Local/data/capstone/Data/GIS_PACKAGE_FILES_TO_CSV/'
+file_direct = "/Users/tina/Documents/School/Capstone/Dallas Files/GIS_PACKAGE_FILES_TO_CSV/"
+#file_direct = 'C:/SMU_Local/data/capstone/Data/GIS_PACKAGE_FILES_TO_CSV/'
 
 # add building permit features
 building_permits = read.csv(file=paste0(file_direct, 'Clipped_Parcels_by_Dallas_Simple_inner_join_to_Clipped_2019_Build_Perm.csv'), stringsAsFactors = FALSE)
@@ -16,12 +16,17 @@ building_permit_features = building_permits %>%
   add_tally(name="count_permits") %>%
   filter(PermitDate == max(PermitDate)) %>%
   ungroup() %>%
-  mutate(count_permits_scaled = normalize(count_permits, method="scale")) %>%
   mutate(days_since_permit =  as.numeric(as.Date("2021-01-01") - Permit_Date, units="days")) %>%
-  mutate(days_since_permit_scaled =  normalize(days_since_permit, method="scale")) %>%
   mutate(permit_type = as.numeric(factor(Permit_Type))) %>%
+  group_by(Acct) %>%
+  summarise(permit_type=median(permit_type), count_permits=median(count_permits), 
+            days_since_permit=median(days_since_permit)) %>%
+  mutate(count_permits_scaled = normalize(count_permits, method="scale")) %>%
+  mutate(days_since_permit_scaled =  normalize(days_since_permit, method="scale")) %>%
   select(Acct, permit_type, count_permits, count_permits_scaled, days_since_permit, days_since_permit_scaled)
 
+account_counts = building_permit_features %>% count(Acct) %>% arrange(desc(n))
+print(head(account_counts))
 
 # Write the data set to a file
 write.csv(building_permit_features, "../../Data/clean_building_permits.csv")
