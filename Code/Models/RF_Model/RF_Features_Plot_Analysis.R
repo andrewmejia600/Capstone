558eeb829da44f7ebc711b8f464cd43363216452
Andrew Mejia <andrew.mejia600@gmail.com>
2021-06-11 00:06:55 -0500

    made charts from full data

--- Code/Models/RF_Model/RF_Features_Plot_Analysis.R
+++ Code/Models/RF_Model/RF_Features_Plot_Analysis.R
@@ -36,8 +36,6 @@ read_data$num_division_cd = factor(read_data$num_division_cd)
 data = read_data[,c(2:12)]
 
 
-
-
 kde_plots(data,log_impr_val, Class)
 kde_plots(data,log_tot_val, Class)
 kde_plots(data,num_division_cd, Class)
@@ -45,7 +43,23 @@ kde_plots(data,log_land_val, Class)
 kde_plots(data,log_area_sqft, Class)
 
 
+##################################### Full Data 
 
+read_data = read.csv('https://raw.githubusercontent.com/andrewmejia600/Capstone/main/Data/df_log_and_scaled.csv')
+
+read_data$num_division_cd = factor(read_data$num_division_cd)
+
+kde_plots(read_data,num_sptd, factor(vac_par))
+kde_plots(read_data,log_impr_val, factor(vac_par))
+kde_plots(read_data,log_tot_val, factor(vac_par))
+kde_plots(read_data,num_division_cd, factor(vac_par))
+kde_plots(read_data,log_land_val, factor(vac_par))
+kde_plots(read_data, log_area_sqft, factor(vac_par))
+kde_plots(read_data, zoning_buckets, factor(vac_par))
+kde_plots(read_data, CO_sqft_scaled, factor(vac_par))
+kde_plots(read_data, occupancy, factor(vac_par))
+kde_plots(read_data, count_of_311_scaled, factor(vac_par))
+kde_plots(read_data, count_of_crime_scaled, factor(vac_par))
 
 
 
