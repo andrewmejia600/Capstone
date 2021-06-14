94175c2ef26dd8f2b3f0368324c78c22179e2494
Andrew Mejia <andrew.mejia600@gmail.com>
2021-06-13 23:41:33 -0500

    updated full features CF chart

--- Code/EDA/Results_EDA_CF_Plots.R
+++ Code/EDA/Results_EDA_CF_Plots.R
@@ -28,6 +28,7 @@ read_data_RF = read.csv('https://raw.githubusercontent.com/andrewmejia600/Capsto
 
 read_data_XG = read.csv('https://raw.githubusercontent.com/andrewmejia600/Capstone/main/Code/Models/XG_Model/XG_test_out.csv')
 
+read_data_XG_Full = read.csv('https://raw.githubusercontent.com/andrewmejia600/Capstone/main/Code/Models/XG_Model_Full/XG_test_out.csv')
 
 ####################################RF 
 read_data_RF['PAR_LABEL_R'] = factor(ifelse(read_data_RF$VAC_PAR == 1, "VAC", "NVAC"))
@@ -73,5 +74,28 @@ p = ggplot(data = plotTable_XG, mapping = aes(x = Reference, y = Prediction, fil
   xlim(rev(levels(table_XG$Reference)))
 
 p + ggtitle("XG-BOOST Confusion Matrix")
+
+
+############################################################ XG Boost 
+
+read_data_XG_Full['PAR_LABEL_R'] = factor(ifelse(read_data_XG_Full$VAC_PAR == 1, "VAC", "NVAC"))
+read_data_XG_Full['PAR_LABEL_P'] = factor(ifelse(read_data_XG_Full$Tuned_XG_preds_Cut == 1, "VAC", "NVAC"))
+
+table_XG <- data.frame(confusionMatrix(read_data_XG_Full$PAR_LABEL_P, read_data_XG_Full$PAR_LABEL_R)$table)
+
+plotTable_XG = table_XG %>%
+  mutate(outcome = ifelse(table_XG$Prediction == table_XG$Reference, "Good", "Bad")) %>%
+  group_by(Reference) %>%
+  mutate(prop = Freq/sum(Freq))
+
+
+p = ggplot(data = plotTable_XG, mapping = aes(x = Reference, y = Prediction, fill = outcome)) +
+  geom_tile() +
+  geom_text(aes(label = Freq), vjust = .5, fontface  = "bold", alpha = 1) +
+  scale_fill_manual(values = c(Good = "#999999", Bad = "#FF9900")) +
+  theme_minimal() +
+  xlim(rev(levels(table_XG$Reference)))
+
+p + ggtitle("XG-BOOST All Features Confusion Matrix")
  
 
